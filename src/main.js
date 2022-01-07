ca176afa198c488fcb970dbe71b00354f4a4e6ed
kkapchin <kkapchin@yandex.ru>
2022-01-07 20:17:26 +0300

    mocks

--- src/main.js
+++ src/main.js
@@ -1,3 +1,4 @@
+import { getMovies } from './mock/movies';
 import { render } from './utils/render';
 import { createFilmsListTemplate } from './view/films-list';
 import { createFooterStatsTemplate } from './view/footer-statistics';
@@ -5,13 +6,14 @@ import { createNavigationTemplate } from './view/navigation';
 import { createSortTemplate } from './view/sort';
 import { createUserProfileTemplate } from './view/user-profile';
 
+const MOVIES_COUNT = 15;
 const header = document.querySelector('.header');
 const main = document.querySelector('.main');
 const footer = document.querySelector('.footer__statistics');
-
+const movies = getMovies(MOVIES_COUNT);
 
 render(header, createUserProfileTemplate(), 'beforeend');
 render(main, createSortTemplate(), 'afterbegin');
 render(main, createNavigationTemplate(), 'afterbegin');
-render(main, createFilmsListTemplate(), 'beforeend');
+render(main, createFilmsListTemplate(movies), 'beforeend');
 render(footer, createFooterStatsTemplate(), 'afterbegin');
