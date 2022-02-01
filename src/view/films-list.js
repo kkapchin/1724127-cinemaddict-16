362487ae1c82d7b8d66c8d0333d3a5a47a19bc65
kkapchin <kkapchin@yandex.ru>
2022-02-01 22:23:22 +0300

    Добавит наследование простым компонентам

--- src/view/films-list.js
+++ src/view/films-list.js
@@ -1,26 +1,12 @@
-import { createElement } from '../utils/render';
+import AbstractView from './abstract-view';
 
 const createFilmsListTemplate = () => (
   `<div class="films-list__container">
   </div>`
 );
 
-export default class FilmsListView {
-  #element = null;
-
-  get element() {
-    if (!this.#element) {
-      this.#element = createElement(this.template);
-    }
-
-    return this.#element;
-  }
-
+export default class FilmsListView extends AbstractView {
   get template() {
     return createFilmsListTemplate();
-  }
-
-  removeElement() {
-    this.#element = null;
   }
 }
--- src/view/user-profile.js
+++ src/view/user-profile.js
@@ -1,4 +1,4 @@
-import { createElement } from '../utils/render';
+import AbstractView from './abstract-view';
 
 const createUserProfileTemplate = () => (
   `<section class="header__profile profile">
@@ -7,22 +7,8 @@ const createUserProfileTemplate = () => (
   </section>`
 );
 
-export default class UserProfileView {
-  #element = null;
-
-  get element() {
-    if (!this.#element) {
-      this.#element = createElement(this.template);
-    }
-
-    return this.#element;
-  }
-
+export default class UserProfileView extends AbstractView {
   get template() {
     return createUserProfileTemplate();
-  }
-
-  removeElement() {
-    this.#element = null;
   }
 }
